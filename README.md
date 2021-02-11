97ad0ba237d80e0a4c45e76b9fdb4a092e057da2
postolenko <denis.postolenko@gmail.com>
2021-02-11 13:12:03 +0200

    Ant

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # http://postolenko.github.io/ant_studio/index.html
 # http://postolenko.github.io/ant_studio/load.html
+# http://postolenko.github.io/ant_studio/projects.html
+# http://postolenko.github.io/ant_studio/testimonials.html
--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,79 @@ $(document).ready(function() {
   // getCardThumbHeight();
   getImgBoxWrappParams();
 
+  $(".testimonials_wrapp").each(function() {
+    testimonial = $(this).find(".testimonial");
+    testimonial.each(function() {
+      index = $(this).index(".testimonial");
+      if(index % 2 != 0 ) {
+        $(this).addClass("odd");
+        objectName = $(this).find("[data-testimonial-link]").attr('data-testimonial-link');
+        $("[data-testimonial ='"+objectName+"']").addClass("odd");
+      }
+    });
+  });
+
+  $("[data-testimonial-link]").on('click', function(e) {
+    e.preventDefault();
+    objectName = $(this).attr('data-testimonial-link');
+    testimonialBox = $("[data-testimonial = '"+objectName+"']");
+    parent = $(this).closest(".testimonial");
+    if(!$(this).hasClass("hide")) {
+      $(".testimonial_content").slideUp(300);
+      $(".testimonial").removeClass("active");
+      setTimeout(function() {
+        $(".testimonial_content").appendTo(".testimonials_texts");              
+      }, 400);
+      setTimeout(function() {
+        if(!parent.hasClass("active")) {
+          parent.addClass("active");
+          if(parent.hasClass("odd")) {
+            insertAfterElem = parent;
+          } else {
+            insertAfterElem = parent.next(".odd");
+          }
+          $(testimonialBox).insertAfter(insertAfterElem);
+          $(testimonialBox).slideDown(300);
+        }
+      }, 500);
+    } else {
+      parent.removeClass("active");
+      testimonialBox.slideUp(300);
+      setTimeout(function() {
+        testimonialBox.appendTo(".testimonials_texts");              
+      }, 400);
+    }
+  });
+
+  $(".testimonial_content .tes_burger").on('click', function(e) {
+    e.preventDefault();
+    parent = $(this).closest(".testimonial_content");
+    objectName = parent.attr('data-testimonial');
+    testimonialThumb = $("[data-testimonial-link = '"+objectName+"']").closest(".testimonial");
+    parent.slideUp(300);
+    setTimeout(function() {
+      parent.appendTo(".testimonials_texts");
+      testimonialThumb.removeClass("active");
+    }, 500);
+  });
+
+  $(this).keydown(function(eventObject){
+      if (eventObject.which == 27) {
+        $(".testimonial_content").each(function() {
+          if($(this).is(":visible")) {
+            testimonialContent = $(this);
+            objectName = $(this).attr('data-testimonial');            
+            testimonialThumb = $("[data-testimonial-link = '"+objectName+"']").closest(".testimonial");
+            testimonialContent.slideUp(300);
+            testimonialThumb.removeClass("active");
+          }
+        });
+        setTimeout(function() {
+          $(".testimonial_content").appendTo(".testimonials_texts");              
+        }, 500);
+      }
+  });
+
     // if( $(".portfolio_slider").length > 0 ) {
     //     $(".portfolio_slider").not(".slick-initialized").slick({
     //         dots: true,
