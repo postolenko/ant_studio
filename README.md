95502577267560f83548e68e5abed527667af5eb
postolenko <denis.postolenko@gmail.com>
2021-03-13 22:17:51 +0200

    A

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # http://postolenko.github.io/ant_studio/index.html
 # http://postolenko.github.io/ant_studio/load.html
 # http://postolenko.github.io/ant_studio/projects.html
+# http://postolenko.github.io/ant_studio/project.html
 # http://postolenko.github.io/ant_studio/testimonials.html
 # http://postolenko.github.io/ant_studio/brif.html
--- js/scripts.js
+++ js/scripts.js
@@ -118,33 +118,30 @@ $(document).ready(function() {
       }
   });
 
-    // if( $(".portfolio_slider").length > 0 ) {
-    //     $(".portfolio_slider").not(".slick-initialized").slick({
-    //         dots: true,
-    //         arrows: true,
-    //         autoplay: true,
-    //         autoplaySpeed: 4000,
-    //         speed: 1200,
-    //         slidesToShow: 4,
-    //         slidesToScroll: 1,
-    //         // fade: true,
-    //         responsive: [
-    //             {
-    //               breakpoint: 900,
-    //               settings: {
-    //                 slidesToShow: 2,
-    //                 slidesToScroll: 2
-    //               }
-    //             },
-    //             {
-    //               breakpoint: 540,
-    //               settings: {
-    //                 slidesToShow: 1,
-    //                 slidesToScroll: 1
-    //               }
-    //             }
-    //           ]
-    //     });
-    // }
+    if( $(".project_slider").length > 0 ) {
+      $(".project_slider").not(".slick-initialized").slick({
+          dots: false,
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 4000,
+          speed: 1200,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          fade: true
+      });
+    }
+
+    if( $(".slider_2").length > 0 ) {
+      $(".slider_2").not(".slick-initialized").slick({
+          dots: false,
+          arrows: true,
+          autoplay: true,
+          autoplaySpeed: 4000,
+          speed: 1200,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          fade: true
+      });
+    }
 
 });
